71c5c7f85fee06afc089d476c9183d10bce0a584
Enzo Balderrama <enzoxxb@gmail.com>
2024-08-31 18:15:23 -0300

    Update-Clase3-Node

---
+++ Metodologia de Sistemas l/Node/01-Hola-Mundo/app3.js
@@ -0,0 +1,12 @@
+console.log("Inicio del programa");
+setTimeout(() => {
+    console.log("Primer Timeout");
+}, 3000);
+setTimeout(() => {
+    console.log("Segundo Timeout");
+}, 3000);
+setTimeout(() => {
+    console.log("Tercer Timeout");
+}, 3000);
+
+console.log("Fin del programa");
