ab4f789ebc1b05e07cafba763d9eafefb105d889
Angelo Vellar <angelovellar24@gmail.com>
2024-09-07 13:32:42 -0300

    Update: Agregando Clase 03 Profundizando con Sistemas Numéricos y String Parte 2

---
+++ Laboratorio IV/Python/Clase 02/tipo_str.py
@@ -0,0 +1,14 @@
+
+import math
+
+#Profundizando en el tipo String
+#Concatenación automática en Python
+
+
+variable = 'Adios'
+mensaje = 'Hola' 'Alumnos' + variable
+mensaje += ', Terminamos'
+#print(mensaje)
+
+#Usamos la clase help para ayuda o documentacion (built-in)
+help(math.isnan)
