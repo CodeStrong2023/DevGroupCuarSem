2f0307c1df3f9fd6e4185c07071669e6f9853ed4
Marcos Martos <marcosmartostuvi@gmail.com>
2024-09-19 20:49:18 -0300

    Clase 5: Proyecto con Stack PERN por Daniel Guerrero -> Parte 1

---
+++ Laboratorio IV/JavaScript/PERN-stack/src/controllers/auth.controller.js
@@ -0,0 +1,4 @@
+export const signin = (req, res) => res.send("ingresando");
+export const signup = (req, res) => res.send("registrando");
+export const sigout = (req, res) => res.send("cerrando sesión");
+export const profile = (req, res) => res.send("pefil de usuario");
