76281f8505c4097c11914109b794d7d987e283c1
Gabriel <gabiggonzalez719@gmail.com>
2024-09-22 19:13:50 -0300

    Clase 05 Profundizando en String

---
+++ Laboratorio IV/Python/Clase 02/doc_str.py
@@ -0,0 +1,9 @@
+from mi_clase import Miclase
+
+# help(Miclase)
+
+# print(Miclase.__doc__)
+# print(Miclase.__init__.__doc__)
+# print(Miclase.mi_metodo.__doc__)
+# print(Miclase.mi_metodo)
+print(type(Miclase.mi_metodo))
