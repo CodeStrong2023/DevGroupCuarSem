d1938afbc3306dcd373f28182e00f388e7304d6b
Joaquin Esposito <joaquiesposito@gmail.com>
2024-10-24 19:44:56 -0300

    Revert "ADD clase 10 promises Node"
    This reverts commit 764c5ae13c9f387513fbeff4f0c07cc9e8a9812f.

--- Metodologia de Sistemas l/Node/02-Fundamentos_node/async/callback.js
+++ Metodologia de Sistemas l/Node/02-Fundamentos_node/async/callback.js
@@ -1,31 +1,23 @@
-function hola(nombre,miCallback){
-    setTimeout(function(){
-    console.log("hola"+nombre)
-    miCallback(nombre)
-    }, 2000);
-    
-};
-
-function adios(nombre,otroCallback){
-    setTimeout(function(){
-        console.log('adios...'+nombre)
-        otroCallback();
-
-    },2500);
-
+function hola(nombre, miCallback){
+    setTimeout( function() {
+        console.log('Hola ' + nombre);
+        miCallback();
+    }, 1000);   
 }
 
-console.log('iniciando el proceso')
+function adios(nombre, otroCallback){
+    setTimeout(function() {
+        console.log('Adios ' + nombre)
+        otroCallback();
+    }, 1500)
+}
 
-hola(' Carlos ',function(nombre){
-    adios(nombre,function(){
-       console.log('terminando el proceso') 
-    })
-    
+console.log('Iniciando el proceso...');
+ hola('Carlos', function(nombre) {
+    adios(nombre, function(){
+        console.log('Terminando el proceso...');
+    });
 });
 
-//hola('carlos',function(){});
-//adios('carlos',function(){});
-
-
-
+//hola('Carlos', function(){});
+//adios('Carlos', function(){});
