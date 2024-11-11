9bc953647672c86b249cbd1e1ea93ef525ea3f10
Angelo Vellar <angelovellar24@gmail.com>
2024-11-11 08:34:59 -0300

    Eliminando carpeta PERN

--- LaboratorioIV/JavaScript/PERN-stack/frontend/src/components/ProtectecRoute.jsx
+++
@@ -1,7 +0,0 @@
-import {Navigate, Outlet} from 'react-router-dom';
-
-export const ProtectedRoute = ({redirecTo,isAllwed,children}) =>{
-    if(!isAllwed)return <Navigate to={redirecTo} replace/>
-
-    return children ? children :<Outlet />;
-}
