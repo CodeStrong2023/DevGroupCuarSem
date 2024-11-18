b2a85e5a4bf25d26faec92b6024f2436023a2eca
Juan Ignacio Ruiz <76715092+Doobleman@users.noreply.github.com>
2024-11-17 21:09:51 -0300

    Delete Metodologia de Sistemas l/Node/Clase13 directory

--- Metodologia de Sistemas l/Node/Clase13/01-Hola-Mundo/app2.js
+++
@@ -1,4 +0,0 @@
-const saludar = (nombre) =>{ //Funcion de Flecha
-    return `Saludos de ${nombre}`; //Usamos back tic`s (`` se crea con alt+96)
-}//esto es igual a : 'saludo' + nombre;
-console.log(saludar('Ariel'));
